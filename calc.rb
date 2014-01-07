f410d341d0558d5ba4ac0880038ba08021808d41
tsyan <anna.tsykalova@gmail.com>
2014-01-07 17:58:23 -0500

    changed greeting prompt, defined input numbers and operation

--- calc.rb
+++ calc.rb
@@ -1,19 +1,32 @@
 # whoo
 
-PROMPT = "> "
-
-def operation_choices
-	puts "Greetings! I am your calculator."
-	puts "Operations available:"
-	puts "addition (+)"
-	puts "subtraction (-)"
-	puts "multiplication (*)"
-	puts "division (/)"
-	puts "What would you like to do? Integers only please."
-	puts PROMPT
+def greeting
+	puts "Greetings! I am your calculator. I can do operations on two numbers."
 end
 
-operation_choices
+def input_number_1
+	puts "what is your first number?"
+	return gets.chomp.to_i
+end
 
+def input_operation
+	puts "what operation would you like to do? choices: + - * /"
+	return gets.chomp.to_s
+end
 
+def input_number_2
+	puts "what is your second number?"
+	return gets.chomp.to_i
+end
 
+# run the calculator
+
+greeting
+
+number_1 = input_number_1
+
+operation = input_operation
+
+number_2 = input_number_2
+
+puts number_1.to_s + operation.to_s + number_2.to_s
