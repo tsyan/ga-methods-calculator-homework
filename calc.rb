86b4fe2b15366c0dcd504fd26a17599206dd2929
tsyan <anna.tsykalova@gmail.com>
2014-01-07 16:59:49 -0500

    defined prompt and available operations

--- calc.rb
+++ calc.rb
@@ -1,1 +1,19 @@
-# Your code goes here
+# whoo
+
+PROMPT = "> "
+
+def operation_choices
+	puts
+		"Greetings! I am your calculator. \n
+		Operations available: \n
+		addition (+) \n
+		subtraction (-) \n
+		multiplication (*) \n
+		division (/) \n
+		What would you like to do? \n"
+	puts PROMPT
+end
+
+operation_choices
+
+
